1926687fc0777d3282ea205402a70ecf35c797ae
aibasil-ai <aibasil@joshlucky.com>
2026-01-23 22:59:28 +0800

    Keep legacy entry points compatible with Next.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Post from './pages/Post'
-import Posts from './pages/Posts'
-import NotFound from './pages/NotFound'
-import ScrollToTop from './components/ScrollToTop'
-import About from './pages/About'
-import Subscribe from './pages/Subscribe'
-
 function App() {
-  return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/posts/:slug" element={<Post />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/subscribe" element={<Subscribe />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  )
+  return null
 }
 
 export default App
